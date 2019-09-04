45c06e401a83aab8cbbcf64d9782d5f15656d3ba
Naresh Tank <tanknaresh7@gmail.com>
2019-09-04 12:46:22 +0530

    section 9 completed: map application

--- maps/src/Company.ts
+++ maps/src/Company.ts
@@ -1,12 +1,15 @@
 import faker from 'faker';
+import { Mappable } from './CustomMap';
 
-export class Company {
+
+export class Company implements Mappable {
     companyName: string;
     catchPhrase: string;
     location: {
         lat: number;
         lng: number;
     }
+    color: string = 'red';
 
     constructor() {
         this.companyName = faker.company.companyName();
@@ -15,5 +18,15 @@ export class Company {
             lat: parseFloat(faker.address.latitude()),
             lng: parseFloat(faker.address.longitude())
         }
+    }
+
+    markerContent(): string {
+        return `
+        <div>
+            <h3>Comapny Name : ${this.companyName}</h3>
+            <h6>Catch Phrase : ${this.catchPhrase}</h6>
+        </div>
+        
+        `
     }
 }
--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,3 +1,12 @@
+export interface Mappable {
+    location: {
+        lat: number;
+        lng: number;
+    },
+    markerContent(): string,
+    color: string
+}
+
 export class CustomMap {
     private googleMap: google.maps.Map;
 
@@ -11,4 +20,26 @@ export class CustomMap {
                 }
             })
     }
+
+    addMarker(mappable: Mappable): void {
+
+        const marker = new google.maps.Marker({
+            map: this.googleMap,
+            position: {
+                lat: mappable.location.lat,
+                lng: mappable.location.lng
+            }
+        });
+
+        marker.addListener('click', () => {
+            const infoWindow = new google.maps.InfoWindow({
+                content: mappable.markerContent()
+            });
+
+            infoWindow.open(this.googleMap, marker);
+
+        });
+
+    }
+
 }
